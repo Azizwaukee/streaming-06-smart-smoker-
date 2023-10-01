e48efe44d2543c54530c4b36ddd59635caa8a177
Azizwaukee <aamaara3@gmail.com>
2023-10-01 14:34:47 -0500

    Assignment 6

---
+++ FoodA_Consumer.py
@@ -0,0 +1,130 @@
+"""
+    This program listens for work messages contiously. 
+    Start multiple versions to add more workers.  
+
+Author: Amaara Aziz
+Date: Seprtember 29, 2023
+
+"""
+
+import pika
+import sys
+from collections import deque
+
+# Configure logging
+from util_logger import setup_logger
+
+logger, logname = setup_logger(__file__)
+
+# Declare Deque Length
+foodA_deque = deque(maxlen=20)
+
+# define a callback function to be called when a message is received
+def foodA_callback(ch, method, properties, body):
+    """ Define behavior on getting a message."""
+    # decode the binary message body to a string
+    logger.info(f" [x] Received {body.decode()}")
+
+    # Setup Callback Information
+    
+    try: 
+        # Smoker Message
+        foodA_message = body.decode().split(",")
+        # Check for valid temperatures
+        if foodA_message[1] == 'No Reading':
+            # Convert to float
+            foodA_temp = float(foodA_message[1])
+            # Check for valid timestamp
+            foodA_timestamp = foodA_message[0]
+            # Append to Deque
+            foodA_deque.append(foodA_temp)
+            
+            # Check for temperature change from previous temperatures 
+            if len(foodA_deque) >= 20 and foodA_deque:
+                temperature_change = foodA_deque[-1] - foodA_deque[0]
+            else:
+                temperature_change = None
+            
+
+        # Send Confirmation Report
+        logger.info("[X] FoodA Temperature Received and Processed.")
+        # Delete Message from Queue after Processing
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    
+    except Exception as e:
+        logger.error("An Error Occured While Processing FoodA Temperature.")
+        logger.error(f"The error says: {e}")
+    
+
+# define a main function to run the program
+def main(hn: str = "localhost", qn: str = "task_queue"):
+    """ Continuously listen for task messages on a named queue."""
+
+    # when a statement can go wrong, use a try-except block
+    try:
+        # try this code, if it works, keep going
+        # create a blocking connection to the RabbitMQ server
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hn))
+
+    # except, if there's an error, do this
+    except Exception as e:
+        print()
+        logger.error("ERROR: connection to RabbitMQ server failed.")
+        logger.error(f"Verify the server is running on host={hn}.")
+        logger.error(f"The error says: {e}")
+        print()
+        sys.exit(1)
+
+    try:
+        # use the connection to create a communication channel
+        channel = connection.channel()
+
+        # use the channel to declare a durable queue
+        # a durable queue will survive a RabbitMQ server restart
+        # and help ensure messages are processed in order
+        # messages will not be deleted until the consumer acknowledges
+        channel.queue_declare(queue=qn, durable=True)
+
+        # The QoS level controls the # of messages
+        # that can be in-flight (unacknowledged by the consumer)
+        # at any given time.
+        # Set the prefetch count to one to limit the number of messages
+        # being consumed and processed concurrently.
+        # This helps prevent a worker from becoming overwhelmed
+        # and improve the overall system performance. 
+        # prefetch_count = Per consumer limit of unaknowledged messages      
+        channel.basic_qos(prefetch_count=1) 
+
+        # configure the channel to listen on a specific queue,  
+        # use the callback function named callback,
+        # and do not auto-acknowledge the message (let the callback handle it)
+        channel.basic_consume( queue=qn, on_message_callback=foodA_callback, auto_ack=False)
+
+        # print a message to the console for the user
+        logger.info(" [*] Ready for work. To exit press CTRL+C")
+
+        # start consuming messages via the communication channel
+        channel.start_consuming()
+
+    # except, in the event of an error OR user stops the process, do this
+    except Exception as e:
+        print()
+        logger.error("ERROR: something went wrong.")
+        logger.error(f"The error says: {e}")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print()
+        logger.info(" User interrupted continuous listening process.")
+        sys.exit(0)
+    finally:
+        logger.info("\nClosing connection. Have a good day!!.\n")
+        connection.close()
+
+
+# Standard Python idiom to indicate main program entry point
+# This allows us to import this module and use its functions
+# without executing the code below.
+# If this is the program being run, then execute the code below
+if __name__ == "__main__":
+    # call the main function with the information needed
+    main("localhost", "02-food-A")
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # streaming-06-smart-smoker-
+
+
+> The assignment is implement data streaming for smart smoker system. to Get started with RabbitMQ, a message broker, that enables multiple Queue and reads the data from the CSV file at every 30 seconds.
+
+
+## Prerequisites
+
+1. Git
+1. Python 3.7+ (3.11+ preferred)
+1. VS Code Editor
+1. VS Code Extension: Python (by Microsoft)
+1. RabbitMQ Server installed and running locally
+
+## Design and Implement Your Producer and Consumer
+
+In GitHub, create a new repo as streaming-06-smart-smoker
+Add a README.md 
+Clone your repo down to your machine. 
+Add a .gitignore.
+Add the Smoker-temps csv data file to the repo. 
+Create a file for your bbq producer.
+Implemented bbq prod
+Create 3 consumers
+
+## Task 6. Execute the Producer/Consumer
+
+1. bbq_consumer.py (and the tutorial)
+2. smoker_consumer.py , FoodA_Consumer.py and FoodB_Consumer.py
+1. Run the file. It will run, emit a message to the named RabbitMQ queue, and finish.
+3. Execute commands in the Anaconda Prompt terminal to display the message. 
+
+# Markdown and Visual Studio Code!
+!![Alt text](images/All_Terminal.JPG)![Alt text](images/RabbitMQ_queuerunning.JPG)![Alt text](images/![Alt text](images/Queue_producer.JPG)RabbitMQ_queuerunning.JPG)![Alt text](images/FoodA_Consumer.JPG)![Alt text](images/producer_cancel.JPG)
+
+## Reference
+
+- [RabbitMQ Tutorial - Hello, World!](https://www.rabbitmq.com/tutorials/tutorial-one-python.html)
+- [Guided Producer Design](https://nwmissouri.instructure.com/courses/54849/pages/module-5-dot-1-guided-producer-design?wrap=1)
+- [Producer Implementation](https://nwmissouri.instructure.com/courses/54849/pages/module-5-dot-2-guided-producer-implementation?wrap=1)
